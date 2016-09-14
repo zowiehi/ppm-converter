114b3d17d486c6fd1bb56ae0423458d20df51800
Zowie Haugaard <zdh27@nau.edu>
2016-09-14 11:55:33 -0700

    Both conversion types using image buffer

--- ppmwrite.c
+++ ppmwrite.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
   FILE *inFile = fopen(argv[2], "rb");
 
   if(strcmp(outType,"P6")) writeP6(inFile, outFile);
-  else return 0;//writeP3(inFile, outFile);
+  else writeP3(inFile, outFile);
 
   //clean up
   (void) fclose(outFile);
@@ -51,7 +51,7 @@ void writeP6 (FILE *inFile, FILE *outFile){
   PPMImage image;
 
 
-  int read, i, j, counter = 1;
+  int read, i, counter = 1;
   unsigned int width, height, maxColors;
   char red[8], green[8], blue[8];
 
@@ -80,6 +80,8 @@ void writeP6 (FILE *inFile, FILE *outFile){
   //read in the max colors
   read = fscanf(inFile, "%u", &maxColors);
 
+  if(maxColors != 255) perror("Please provide an 24-bit color file");
+
   //put all that good stuff into the outFile
   fprintf(outFile, "%u %u\n%u\n",image.width, image.height, maxColors);
 
@@ -98,58 +100,50 @@ void writeP6 (FILE *inFile, FILE *outFile){
       fwrite(image.data, 3 * image.width, image.height, outFile);
 }
 
-// void writeP3 (FILE *inFile, FILE *outFile){
-//
-//   char buff[SIZE], *fb;
-//
-//   Pix *image;
-//   Pix curPix;
-//
-//   int read, i, j, counter = 1;
-//   unsigned int width, height, colors;
-//   unsigned char red, green, blue;
-//
-//   fb = (char *)malloc(sizeof(char) * SIZE);
-//   fb = fgets(buff, SIZE, inFile);
-//   if ( (fb == NULL) || ( strncmp(buff, "P6\n", 3) != 0 ) ) perror("Please provide a P6 .ppm file for conversion\n");
-//   (void) fprintf(outFile, "P3\n");
-//
-//   do
-//         {
-//            fb = fgets(buff, SIZE, inFile);
-//            if( strncmp(buff, "#", 1) == 0) fprintf(outFile, "%s", buff);
-//            if ( fb == NULL ) return;
-//         } while ( strncmp(buff, "#", 1) == 0 );
-//
-//   read = sscanf(buff, "%u %u", &width, &height);
-//
-//
-//     image = (Pix *)malloc(sizeof(Pix) * width * height);
-//
-//   if(read < 2) perror("File Unreadable. Please check the file format\n");
-//   read = fscanf(inFile, "%u", &colors);
-//
-//   fprintf(outFile, "%u %u\n%u\n",width, height, colors);
-//   for (i = 0; i < width*height; i++)
-//   {
-//
-//       fread(curPix, 3, 1, inFile);
-//
-//       image[i] = curPix;
-//   }
-//
-//   for (i = 0; i < width*height; i++)
-//   {
-//     curPix = image[i];
-//     fprintf(outFile, " %d %d %d ", curPix[1], curPix[2], curPix[0]);
-//
-//     if (counter == width)
-//       {
-//           fprintf(outFile, "\n");
-//           counter = 1;
-//       }
-//
-//       else
-//           counter++;
-//   }
-// }
+void writeP3 (FILE *inFile, FILE *outFile){
+
+  char buff[SIZE], *fb;
+
+  PPMImage image;
+
+  int read, i, j, counter = 1;
+  unsigned int width, height, maxColors;
+  unsigned char red, green, blue;
+
+  fb = (char *)malloc(sizeof(char) * SIZE);
+  fb = fgets(buff, SIZE, inFile);
+  if ( (fb == NULL) || ( strncmp(buff, "P6\n", 3) != 0 ) ) perror("Please provide a P6 .ppm file for conversion\n");
+  (void) fprintf(outFile, "P3\n");
+
+  do
+        {
+           fb = fgets(buff, SIZE, inFile);
+           if( strncmp(buff, "#", 1) == 0) fprintf(outFile, "%s", buff);
+           if ( fb == NULL ) return;
+        } while ( strncmp(buff, "#", 1) == 0 );
+
+  //read in the width and height
+  read = sscanf(buff, "%u %u", &image.width, &image.height);
+
+  image.data = (RGBPix *)malloc(sizeof(RGBPix) * image.width * image.height);
+
+  if(read < 2) perror("File Unreadable. Please check the file format\n");
+  read = fscanf(inFile, "%u", &maxColors);
+
+  //put all that good stuff into the outFile
+  fprintf(outFile, "%u %u\n%u\n",image.width, image.height, maxColors);
+
+  fread(image.data, sizeof(RGBPix), image.width * image.height, inFile);
+
+  for (i = 0; i < image.width * image.height; i++)
+  {
+      fprintf(outFile, " %d %d %d ", image.data[i].g, image.data[i].b, image.data[i].r);
+
+      if(counter == image.width) {
+        fprintf(outFile, "\n");
+        counter = 1;
+      }
+      else counter += 1;
+    }
+
+}
